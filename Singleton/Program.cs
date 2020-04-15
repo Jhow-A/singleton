b82a1d0689485c7965e2ad58726af42ba23784aa
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-04-15 11:18:51 -0300

    Testando Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ClasseNormal = new ClasseNormal { variavelClasseNormal = 26};
+            Console.WriteLine("Uso de classe Normal: " + ClasseNormal.variavelClasseNormal);
+
+            string valorRetornado = ClasseEstatica.ExibirValor();
+            Console.WriteLine("Uso de classe Static: " + valorRetornado);
+
+            var variavelSingleton = ClasseSingleton.Instancia;
+            variavelSingleton.Mensagem = "Teste";
+            Console.WriteLine("Uso de classe Singleton: " + variavelSingleton.Mensagem);
+
+            //Teste de instâncias Singleton
+            var outraVariavelSingleton = ClasseSingleton.Instancia;
+            Console.WriteLine("Verifcando se as instâncias são as mesmas: " + variavelSingleton.Equals(outraVariavelSingleton));
+            Console.WriteLine(outraVariavelSingleton.Mensagem);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class ClasseNormal
+    {
+        public int variavelClasseNormal;
+    }
+
+    class ClasseSingleton
+    {
+        static ClasseSingleton _instancia;
+
+        public static ClasseSingleton Instancia
+        {
+            get { return _instancia ?? (_instancia = new ClasseSingleton()); }
+        }
+
+        private ClasseSingleton() { }
+
+        public string Mensagem { get; set; }
+    }
+
+    static public class ClasseEstatica
+    {
+        private static readonly int variavelEstatica;
+
+        // Construtor Static: é executado somente uma vez quando o tipo for usado.
+        static ClasseEstatica()
+        {
+            variavelEstatica = 1;
+        }
+
+        public static string ExibirValor()
+        {
+            return string.Format("O valor da variável estática é {0}", variavelEstatica);
+        }
+
+        public static string Mensagem { get; set; }
+    }
+}
